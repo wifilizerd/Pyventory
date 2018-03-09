2e8667dc448f968c4c396d4a35d0445c41a282be
wboy4 <william.boy4@gmail.com>
2018-03-09 14:50:07 -0700

    Export bug fix no more missinginfo.csv
    export command was not working when you select cetin files. also removed the MissingINFO output.

--- pyventory.py
+++ pyventory.py
@@ -31,7 +31,6 @@ def Help(_menu):                                                #The Help Screen
             "add        enable taking notes if the recorde is not found in inventory file.\n"
             "notadd     disable taking notes if the recorde is not found in inventory file.\n"
             "ask        ask each time to take notes if the recorde is not found in inventory file.\n"
-            "export     run Check and Export using the same inventory file used in Scan and Check.\n"
         )   
     elif _menu == 'export':                                         #CheckandExport Help Menu
         pass
@@ -47,13 +46,13 @@ def CSVReader(_filename):                                       #Setup CSV reade
     _file = open(_filename, "r")
     return(csv.reader(_file))
   
-def Convert2List(_filename, _column, _savefile):                           #Read data from file and add it to a list.
+def Convert2List(_filename, _column):                           #Read data from file and add it to a list.
     _filereader = CSVReader(_filename)
     _filelist = []                                                  #set blank list to recive data
     for row in list(_filereader):
         if row:                                                     #check if there is data on tha row.
             if row[int(_column)] == '':                                  #check if there is data in the field
-                writefile(_savefile, 'csv', row)                 #write out any rows that do not have info in the selected column
+                pass
             else:
                 _filelist.append(row)                       #appedn data to list in UPPER case
     return(_filelist)
@@ -169,7 +168,7 @@ def FileBrowser(_extention, _new):                              #Used to select 
 
 def columnselect(_filename, _savefile):                         #lists info in CSV file from the first row so user can select the column based on content.
     _count = 0
-    _INVlist = Convert2List(_filename, 0, _savefile)
+    _INVlist = Convert2List(_filename, 0)
     print("\033[1;32m" + 'Select the Column you wish to use.')      #listing help info of known column from inventory files.
     print('Known Coumns for defult exports ONLY')
     print('0 - Asset tages')
@@ -238,14 +237,15 @@ def SCANandCHECK():                                             #Interface for s
     ' ___  ___   _   _  _                _    ___ _  _ ___ ___ _  __ \n'
     '/ __|/ __| /_\ | \| |  __ _ _ _  __| |  / __| || | __/ __| |/ / \n'
     '\__ | (__ / _ \| .` | / _` |   \/ _` | | (__| __ | _| (__| | <  \n'
-    '|___/\___/_/ \_|_|\_| \__,_|_||_\__,_|  \___|_||_|___\___|_|\_\ v0.3'                                                               
+    '|___/\___/_/ \_|_|\_| \__,_|_||_\__,_|  \___|_||_|___\___|_|\_\ \n'                                                               
     + "\033[1;37m") #white
     print('')
+    print('Ask each time to take notes')
+    print('Type "help" for help menu')
     print('press "x" then enter to exit')
-    print('Noteing Recorde is disabled, to enable type "add" then enter')
     
     _assetLIST = []
-    for row in Convert2List(_INVfile, _INVcol, _Savefile):                     # convert list of lists into a single list of specified column info.
+    for row in Convert2List(_INVfile, _INVcol):                     # convert list of lists into a single list of specified column info.
         _assetLIST.append(row[int(_INVcol)].lstrip('0'))
 
     _scan = 0
@@ -253,20 +253,20 @@ def SCANandCHECK():                                             #Interface for s
         _scan = raw_input(':')
         if _scan.lower() == 'add':                                  #add command used to turn on adding info if entery is not found.
             _ADD = 1
-            print('Noteing recorde is enabled')
+            print('Taking notes is ENABLED')
         elif _scan.lower() == 'notadd':                             #notadd commadn used to turn off adding info in entery in not found
             if _ADD == 0:
-                print('Noting already disabled')
+                print('Taking notes is DISABLED')
             else:
                 _ADD = 0
-                print('Noteing recorde is disabled')
+                print('Taking notes is DISABLED')
         elif _scan.lower() == 'ask':                                #ask command used to set program to ask user each time a record is not found if info should be added.
             _ADD = 2
-            print('Will ask to note on any recorde that is not found')
+            print('Ask each time to take notes')
         elif _scan.lower() == 'help':                               #help commadn used to show the help screen
-            pass
+            Help('scan')
         elif _scan.lstrip('0') in _assetLIST or _scan in _assetLIST:#compaire of entery and inventory info.
-            for row in Convert2List(_INVfile, _INVcol, _Savefile):
+            for row in Convert2List(_INVfile, _INVcol):
                 if row[int(_INVcol)] == _scan.lstrip('0') or row[int(_INVcol)] == _scan:
                     print("\033[1;32m" + str(row) + "\033[1;37m")
                     writefile((_Savefile + "-scanned"),'csv', row)
@@ -296,6 +296,9 @@ def CheckandExport():                                           #Interface to Ch
         os.system('cls')
     print("\033[1;32m" + 'Select your Scanned file.' + "\033[1;37m")
     _scanfile = FileBrowser('.csv', 0)
+    if _DEBUG > 0:
+        print(_scanfile)
+        pause=raw_input(':')
     if _DEBUG < 1:
         os.system('cls')
     _scancol = columnselect(_scanfile, _scanfile) 
@@ -319,13 +322,13 @@ def CheckandExport():                                           #Interface to Ch
     "   ___ _           _                  _   ___                   _   \n"
     "  / __| |_  ___ __| |__  __ _ _ _  __| | | __|_ ___ __  ___ _ _| |_ \n"
     " | (__| ' \/ -_) _| / / / _` | ' \/ _` | | _|\ \ / '_ \/ _ \ '_|  _|\n"
-    "  \___|_||_\___\__|_\_\ \__,_|_||_\__,_| |___/_\_\ .__/\___/_|  \__| v0.3\n"   
+    "  \___|_||_\___\__|_\_\ \__,_|_||_\__,_| |___/_\_\ .__/\___/_|  \__|\n"   
     "                                                 |_|                "                                                            
     + "\033[1;37m")
     print('')
-    print('press x then enter to exit')
-    _INVlist = Convert2List(_INVfile, _INVcol, _scanfile[:-12])
-    _scanlist = Convert2List(_scanfile, _scancol, _scanfile[:-12])
+    print('Please wait, Checking recordes')
+    _INVlist = Convert2List(_INVfile, _INVcol)
+    _scanlist = Convert2List(_scanfile, _scancol)
 
     _scancollist = []
     for row in _scanlist:
@@ -341,7 +344,6 @@ def CheckandExport():                                           #Interface to Ch
             pass
 
 def Interface():
-    _interface = 0
     while True:
         os.system('cls')
         Logo()
